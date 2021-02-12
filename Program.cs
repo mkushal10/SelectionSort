031c31e89f36b53df4f28cb4bbaa54e8b66d6d7a
Kushal Neupane <mkushal10@gmail.com>
2021-02-12 18:28:53 +0545

    1st commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[55];
+            int i, j, n, x, y, temp;
+
+            Console.WriteLine("Enter the Size of Array :");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Array Elements :");
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse((Console.ReadLine()));
+            }
+            for (i = 0; i < (n - 1); i++)
+            {
+                y = arr[i];
+                x = i;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (y > arr[j])
+                    {
+                        y = arr[j];
+                        x = j;
+                    }
+                }
+                temp = arr[i];
+                arr[i] = arr[x];
+                arr[x] = temp;
+            }
+            Console.WriteLine("After Selection Sort :");
+            for (i = 0; i < n; i++)
+                Console.WriteLine(arr[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
